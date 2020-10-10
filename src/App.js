671a442c40283036d54d412ca68d36f70298e1d3
Rishika_codes <gangashettyrishika24@gmail.com>
2020-10-10 10:41:37 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './homepage.component';
+
+
+function App() {
+  return <div>
+    <Homepage></Homepage>
+  </div>
+}
+
+export default App;
